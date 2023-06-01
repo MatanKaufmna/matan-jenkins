d464607774800ed1117aefef6f2f8dea2a3b32ee
matan <matankaufman1@gmail.com>
2023-06-01 19:21:35 +0300

    add fanntastic and boto3

--- 15_fantastic_ascii/setup.py
+++ 15_fantastic_ascii/setup.py
@@ -1,22 +1,33 @@
 from setuptools import setup, find_packages
 
-yt_dlp_package_setup_args = dict(
-    name='yt_dlp_package',
-    version='1.0.0',
-    description='yt-dlp Package',
+fantastic_ascii_setup_args = dict(
+    name='fantastic_ascii',
+    version='2.0.0',
+    description='Fantastic ASCII',
     license='MIT',
     install_requires=[
-        'yt-dlp>=2023.2.17',
-        'boto3',
-        'loguru',
-        'pyTelegramBotAPI',
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+    author='Matt',
+    author_email='example@example.com'
+)
 
+boto3_package_setup_args = dict(
+    name='boto3_package',
+    version='1.0.0',
+    description='Boto3 Package',
+    license='MIT',
+    install_requires=[
+        'boto3',
     ],
     author='Matt',
     author_email='example@example.com'
 )
 
 
+
 if __name__ == '__main__':
-    setup(**yt_dlp_package_setup_args)
+    setup(**fantastic_ascii_setup_args)
+    setup(**boto3_package_setup_args)
 
