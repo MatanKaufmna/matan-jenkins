573886579dc0d5c712f0e00221c8bd840fe6670b
matan <matankaufman1@gmail.com>
2023-06-01 19:14:25 +0300

    add yt_dlp_package_setup_args

--- 15_fantastic_ascii/setup.py
+++ 15_fantastic_ascii/setup.py
@@ -1,24 +1,22 @@
 from setuptools import setup, find_packages
 
-fantastic_ascii_setup_args = dict(
-    name='fantastic_ascii',
-    version='2.0.0',
-    description='Fantastic ASCII',
+yt_dlp_package_setup_args = dict(
+    name='yt_dlp_package',
+    version='1.0.0',
+    description='yt-dlp Package',
     license='MIT',
     install_requires=[
-        'requests',
-        'importlib-metadata; python_version == "3.8"',
-        'boto3',
         'yt-dlp>=2023.2.17',
-        'loguru'
+        'boto3',
+        'loguru',
         'pyTelegramBotAPI',
+
     ],
     author='Matt',
     author_email='example@example.com'
 )
 
 
-
 if __name__ == '__main__':
-    setup(**fantastic_ascii_setup_args)
+    setup(**yt_dlp_package_setup_args)
 
