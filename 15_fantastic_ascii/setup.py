63e52cd470b3762f7760f48f1a00fc314df2e5a0
matan <matankaufman1@gmail.com>
2023-06-01 18:05:17 +0300

    add boto3 loguru and more to Nexus

--- 15_fantastic_ascii/setup.py
+++ 15_fantastic_ascii/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup_args = dict(
+fantastic_ascii_setup_args = dict(
     name='fantastic_ascii',
     version='2.0.0',
     description='Fantastic ASCII',
@@ -8,12 +8,62 @@ setup_args = dict(
     install_requires=[
         'requests',
         'importlib-metadata; python_version == "3.8"',
-        'pip'
     ],
     author='Matt',
     author_email='example@example.com'
 )
 
+boto3_package_setup_args = dict(
+    name='boto3_package',
+    version='1.0.0',
+    description='Boto3 Package',
+    license='MIT',
+    install_requires=[
+        'boto3',
+    ],
+    author='Matt',
+    author_email='example@example.com'
+)
+
+yt_dlp_package_setup_args = dict(
+    name='yt_dlp_package',
+    version='1.0.0',
+    description='yt-dlp Package',
+    license='MIT',
+    install_requires=[
+        'yt-dlp>=2023.2.17',
+    ],
+    author='Matt',
+    author_email='example@example.com'
+)
+
+loguru_package_setup_args = dict(
+    name='loguru_package',
+    version='1.0.0',
+    description='Loguru Package',
+    license='MIT',
+    install_requires=[
+        'loguru',
+    ],
+    author='Matt',
+    author_email='example@example.com'
+)
+
+pytelegrambotapi_package_setup_args = dict(
+    name='pytelegrambotapi_package',
+    version='1.0.0',
+    description='pyTelegramBotAPI Package',
+    license='MIT',
+    install_requires=[
+        'pyTelegramBotAPI',
+    ],
+    author='Matt',
+    author_email='example@example.com'
+)
 
 if __name__ == '__main__':
-    setup(**setup_args)
+    setup(**fantastic_ascii_setup_args)
+    setup(**boto3_package_setup_args)
+    setup(**yt_dlp_package_setup_args)
+    setup(**loguru_package_setup_args)
+    setup(**pytelegrambotapi_package_setup_args)
