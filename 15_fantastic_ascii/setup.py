ee332768cd718b7389bda07939a289c051ea584d
matan <matankaufman1@gmail.com>
2023-06-01 19:27:05 +0300

    boto3 and ytdlp

--- 15_fantastic_ascii/setup.py
+++ 15_fantastic_ascii/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 
-
 boto3_package_setup_args = dict(
     name='boto3_package',
     version='1.0.0',
@@ -9,6 +8,18 @@ boto3_package_setup_args = dict(
     license='MIT',
     install_requires=[
         'boto3',
+    ],
+    author='Matt',
+    author_email='example@example.com'
+)
+
+yt_dlp_package_setup_args = dict(
+    name='yt_dlp_package',
+    version='1.0.0',
+    description='yt-dlp Package',
+    license='MIT',
+    install_requires=[
+        'yt-dlp>=2023.2.17',
     ],
     author='Matt',
     author_email='example@example.com'
